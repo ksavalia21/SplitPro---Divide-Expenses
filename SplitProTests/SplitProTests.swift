0fe0a6227b838a47e54ad2676fa96e348e0f4a5d
ksavalia21 <savalia.keyur123@gmail.com>
2025-10-12 17:30:03 -0700

    Initial Commit

---
+++ SplitProTests/SplitProTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplitProTests.swift
+//  SplitProTests
+//
+//  Created by Keyur Savalia on 10/12/25.
+//
+
+import Testing
+@testable import SplitPro
+
+struct SplitProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
